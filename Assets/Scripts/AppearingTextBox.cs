f1eb90f3e27fa4b71258d4c5faa9e441bcc12663
Audrey Man <82979811+MournTHEMorning@users.noreply.github.com>
2024-08-09 14:13:50 -0400

    [T-no_show]TextBox appear code implemented
    TextBox appear code implemented
    **text not showing up, text bg is temp/test asset

--- Assets/Scripts/AppearingTextBox.cs
+++ Assets/Scripts/AppearingTextBox.cs
@@ -2,23 +2,56 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+namespace RobotPlayer{
 //Textbox appears when player gets close to npc
 public class AppearingTextBox : MonoBehaviour
 {
     [SerializeField] private GameObject player;
 
-    [Tooltip("At what distance should the textbox appear?")]
-    [SerializeField] private float range;
+    [Tooltip("At what distance should the textbox appear? 2 is minimum value given 32x32 tile")]
+    [SerializeField] private float range = 2.0f;
+
+    [Tooltip("Put Gameobject that will appear/dissappear")]
+    public GameObject textbox;
+
+    //checks if everything is present before checking stuff
+    private bool diagnosticsOkay;
 
     // Start is called before the first frame update
-    void Start()
+    public void Start()
     {
-        
+        if(!player){
+            Debug.Log("No Player reference; put player prefab into inspector");
+            diagnosticsOkay = false;
+        }
+
+        if(!textbox){
+            Debug.Log("No Textbox reference; put npc's textbox into inspector");
+            diagnosticsOkay = false;
+
+        }
+
+        if(range < 2.0){
+            Debug.Log("Comparing distance (aka Range) must exist and be 2>=");
+            diagnosticsOkay = false;
+        }
+
+        diagnosticsOkay = true;
+       
     }
 
-    // Update is called once per frame
-    void Update()
+    public void FixedUpdate()
     {
-        
+        if(diagnosticsOkay){
+            CheckDistance();
+        }
+
     }
+
+    private void CheckDistance(){
+        //if the distance between the two is greater or equal, then set active or deactivate
+        textbox.SetActive((Vector3.Distance((Vector3)this.transform.position, player.transform.position) <= range));
+    }
+
+}
 }
